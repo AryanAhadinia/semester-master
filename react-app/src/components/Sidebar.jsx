ec176ddc1c6db73aa6b0e2accf3a2eab8cd985c8
Mohammadjafari80 <mamad.jafari91@gmail.com>
2021-02-11 16:34:41 +0330

    add shadow and border

--- react-app/src/components/Sidebar.jsx
+++ react-app/src/components/Sidebar.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Col } from 'react-bootstrap';
 import person from '../person.jpg'
 import MenuItem from './MenuItem';
+import Tilt from 'react-parallax-tilt';
+
 
 class Sidebar extends Component {
     state = {  
@@ -12,7 +14,9 @@ class Sidebar extends Component {
     render() { 
         return (
             <div className="drop sidebar" style={{width : '200px'}}>
-                <img src={person} class="rounded-circle mx-auto d-block" alt="..." style={{width : '50%', paddingTop : '25%', paddingBottom : '25%'}}></img>
+                <Tilt>
+                <img src={person} class="rounded-circle mx-auto d-block" alt="..." style={{width : '50%'}}></img>
+                </Tilt>
                 <MenuItem item='داشبورد' selected={this.state.currentState === 1} onClick={() => this.handleCurrentState(1)}></MenuItem>
                 <MenuItem item='برنامه ریزی' selected={this.state.currentState === 2} onClick={() => this.handleCurrentState(2)}></MenuItem>
                 <MenuItem item='جدول دروس' selected={this.state.currentState === 3} onClick={() => this.handleCurrentState(3)}></MenuItem>
