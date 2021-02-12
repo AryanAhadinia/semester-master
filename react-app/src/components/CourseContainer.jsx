3d32b5b3db5c1e9bca43a12e91b1ed3a614c3a0f
Mohammadjafari80 <mamad.jafari91@gmail.com>
2021-02-12 17:53:12 +0330

    add course container component

---
+++ react-app/src/components/CourseContainer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CourseContainer extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div className="courses-container">
+                
+            </div>
+          );
+    }
+}
+ 
+export default CourseContainer;
--- react-app/src/components/Departments.jsx
+++ react-app/src/components/Departments.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import Course from './CourseContainer';
 
 class Departments extends Component {
     state = {  }
     render() { 
         return (
-        <div className='department-div'>
+        <div className='department-div d-flex flex-column justify-content-start align-items-center'>
             <select class="custom-select custom-select-lg mb-3">
                 <option selected>دانشکده</option>
                 <option value="1">برق</option>
                 <option value="2">شیمی</option>
                 <option value="3">کامپیوتر</option>
             </select>
+            <Course>
+
+            </Course>
         </div>
         );
     }
