e718eee5eb8ddeb1f76f18d6e40789b7c11bb608
Mohammadjafari80 <mamad.jafari91@gmail.com>
2021-02-14 15:12:38 +0330

    add routing

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,66 +1,100 @@
-import React, { Component } from "react";
-import Sidebar from "./components/Sidebar";
-import Card from "./components/Card";
-import { Col, Row } from "react-bootstrap";
-import Week from "./components/Week";
-import logo from "./termix.png";
-import Departments from "./components/Departments";
-import TableContainer from "./components/TableContainer";
-import Timetable from "./components/Timetable";
+import React, { Component } from 'react';
+import Sidebar from './components/Sidebar';
+import Card from './components/Card';
+import { Col, Row } from 'react-bootstrap';
+import Week from './components/Week';
+import logo from './termix.png';
+import Departments from './components/Departments';
+import TableContainer from './components/TableContainer';
+import Timetable from './components/Timetable';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect,
+} from 'react-router-dom';
 
 class App extends Component {
-    state = {
-        currentState: 1,
-    };
+	state = {
+		currentState: 1,
+	};
 
-    render() {
-        return (
-            <React.Fragment>
-                <div className="d-flex flex-row flex-fill h-100">
-                    <Sidebar
-                        handleCurrentState={this.handleCurrentState}
-                        currentState={this.state.currentState}
-                    ></Sidebar>
-                    <Col className="d-flex flex-column justify-content-start align-items-center flex-fill main-section">
-                        {/* <img id="termix-logo" src={logo} alt="Termix"></img> */}
-                        <div
-                            className="d-flex justify-content-between w-100 h-100"
-                            style={{ margin: "15px auto" }}
-                        >
-                            {this.handleSidebar()}
-                            {this.state.currentState === 2 ? (
-                                <div className="flex-grow-1 flex-shrink-1">
-                                    <Departments></Departments>
-                                </div>
-                            ) : null}
-                        </div>
-                    </Col>
-                </div>
-            </React.Fragment>
-        );
-    }
+	render() {
+		return (
+			<Router>
+				<React.Fragment>
+					<div className='d-flex flex-row flex-fill h-100'>
+						<Sidebar
+							handleCurrentState={this.handleCurrentState}
+							currentState={this.state.currentState}></Sidebar>
+						<Col className='d-flex flex-column justify-content-start align-items-center flex-fill main-section'>
+							<div
+								className='d-flex justify-content-between w-100 h-100'
+								style={{ margin: '15px auto' }}>
+								{/* {this.handleSidebar()} */}
+								<Switch>
+									<Route
+										path='/dashboard'
+										render={() => this.handleSidebar()}
+									/>
+									<Route
+										path='/timetable'
+										render={() => this.handleSidebar()}
+									/>
+									<Route
+										path='/courseTable'
+										render={() => this.handleSidebar()}
+									/>
+									<Route
+										path='/'
+										render={() => this.handleSidebar()}>
+										<Redirect to='/dashboard'></Redirect>
+									</Route>
+								</Switch>
+							</div>
+						</Col>
+					</div>
+				</React.Fragment>
+			</Router>
+		);
+	}
 
-    handleCurrentState = (state) => {
-        this.setState({ currentState: state });
-    };
+	handleCurrentState = (state) => {
+		this.setState({ currentState: state });
+	};
 
-    handleSidebar = () => {
-        switch (this.state.currentState) {
-            case 1:
-                return (
-                    <div className="w-100">
-                        <Card></Card>
-                    </div>
-                );
-            case 2:
-                // return <Week></Week>;
-                return <Timetable></Timetable>;
-            case 3:
-                return <TableContainer></TableContainer>;
-            default:
-                return null;
-        }
-    };
+	handleSidebar = () => {
+		switch (this.state.currentState) {
+			case 1:
+				return (
+					<React.Fragment>
+						<Redirect to='/dashboard'></Redirect>
+						<div className='w-100'>
+							<Card></Card>
+						</div>
+					</React.Fragment>
+				);
+			case 2:
+				return (
+					<React.Fragment>
+						<Redirect to='/timetable'></Redirect>
+						<Timetable></Timetable>
+						<div className='flex-grow-1 flex-shrink-1'>
+							<Departments></Departments>
+						</div>
+					</React.Fragment>
+				);
+			case 3:
+				return (
+					<React.Fragment>
+						<Redirect to='/courseTable'></Redirect>
+						<TableContainer></TableContainer>
+					</React.Fragment>
+				);
+			default:
+				return null;
+		}
+	};
 }
 
 export default App;
