68d248ace084de142693818bc25a414daada73fc
Mohammadjafari80 <mamad.jafari91@gmail.com>
2021-02-14 17:38:31 +0330

    make sidebar responsive

--- react-app/src/components/Sidebar.jsx
+++ react-app/src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../termix.png'
+import smallLogo from '../termix-icon.svg'
+
 import MenuItem from './MenuItem';
 import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
 import { faChartPie } from "@fortawesome/free-solid-svg-icons";
@@ -16,7 +18,8 @@ class Sidebar extends Component {
     render() { 
         return (
             <div className="drop sidebar" style={{width : '200px'}}>
-                <img src={logo} className="mx-auto d-block" alt="termix" ></img>
+                <img src={logo} className="big-logo" alt="termix" ></img>
+                <img src={smallLogo} className='small-logo' alt="termix" ></img>
                 <MenuItem icon={faAddressCard} item='داشبورد' selected={this.props.currentState === 1} onClick={() => this.props.handleCurrentState(1)}></MenuItem>
                 <MenuItem icon={faChartPie} item='برنامه ریزی' selected={this.props.currentState === 2} onClick={() => this.props.handleCurrentState(2)}></MenuItem>
                 <MenuItem icon={faTable} item='جدول دروس' selected={this.props.currentState === 3} onClick={() => this.props.handleCurrentState(3)}></MenuItem>
