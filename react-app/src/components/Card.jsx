491bd3f3be535e46ae045977a4607a7ea8039627
Mohammadjafari80 <mamad.jafari91@gmail.com>
2021-02-18 01:46:26 +0330

    fix card functionality

--- react-app/src/components/Card.jsx
+++ react-app/src/components/Card.jsx
@@ -5,9 +5,28 @@ import person from '../person.jpg';
 import Tilt from 'react-parallax-tilt';
 import '../index.css';
 import imam from '../imamQoute.png';
+import db from '../services/db';
+import userService from '../services/userService';
 
 class Card extends Component {
-	state = {};
+	state = {
+		user: {},
+	};
+
+	constructor() {
+		super();
+		this.init();
+	}
+
+	async init() {
+		const isThereMyUser = await db.userInfo.count();
+		if (isThereMyUser === 0) {
+			const { data: user } = await userService.getMyUserInfo();
+			await userService.saveUserInfo(user);
+		}
+		const myUser = await db.userInfo.get(0);
+		this.setState({ user: myUser });
+	}
 	render() {
 		return (
 			<React.Fragment>
@@ -59,8 +78,9 @@ class Card extends Component {
 												className='form-control info-field'
 												id='inputName'
 											>
-												محمد
-												</label>
+												{this.state.user &&
+													this.state.user.lastName}
+											</label>
 										</div>
 										<div className='form-group'>
 											<label htmlFor='exampleInputEmail1'>
@@ -71,7 +91,8 @@ class Card extends Component {
 												className='form-control info-field'
 												id='inputName'
 											>
-												جعفری
+												{this.state.user &&
+													this.state.user.firstName}
 											</label>
 										</div>
 										<div className='form-group'>
@@ -83,7 +104,8 @@ class Card extends Component {
 												className='form-control info-field'
 												id='inputName'
 											>
-												98105654
+												{this.state.user &&
+													this.state.user.stdId}
 											</label>
 										</div>
 										<div className='form-group'>
@@ -94,7 +116,7 @@ class Card extends Component {
 												className='info-field form-control'
 												id='exampleFormControlSelect1'
 											>
-												مهندسی کامپیوتر
+												کامپیوتر
 											</label>
 										</div>
 										<div className='form-group'>
@@ -106,7 +128,9 @@ class Card extends Component {
 												className='form-control info-field'
 												id='inputName'
 											>
-												کارشناسی
+												{!this.state.user.grade
+													? 'کارشناسی'
+													: 'تحصیلات تکمیلی'}
 											</label>
 										</div>
 									</Form>
